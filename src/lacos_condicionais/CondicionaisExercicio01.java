d833f720a580baa39337943620baafea128ffb5c
denizafontineles <denizafontineles@outlook.com>
2024-02-28 09:35:10 -0300

    Adiciona Aula e Exercicios de Laço de Repetição

---
+++ src/lacos_condicionais/CondicionaisExercicio01.java
@@ -0,0 +1,30 @@
+package lacos_condicionais;
+
+import java.util.Scanner;
+
+public class CondicionaisExercicio01 {
+
+	public static void main(String[] args) {
+		
+		int a, b, c, soma;
+		
+		Scanner read = new Scanner(System.in);
+		
+		System.out.print("Digite um valor inteiro de A: ");
+		a = read.nextInt();
+		System.out.print("Digite um valor inteiro de B: ");
+		b = read.nextInt();
+		System.out.print("Digite um valor inteiro de C: ");
+		c = read.nextInt();
+		
+		soma = a + b;
+		
+		if (soma >= c) {
+			System.out.println("\nA soma entre A + B é maior do que C");
+			System.out.println("\nValor de C = " + c +" -> Resultado da soma: " + soma);
+		}else {
+			System.out.println("\nA soma entre A + B NÃO é maior do que C");
+			System.out.println("\nValor de C = " + c + " -> Resultado da soma: " + soma);
+		}
+	}
+}
